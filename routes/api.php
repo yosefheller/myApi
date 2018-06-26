ac6be57d65858b96cfe1b8b580c5921e7a81ae8c
vagrant <vagrant@homestead>
2018-06-26 19:28:20 +0000

    added doc

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::view('/', 'welcome');
 Route::resource('institutions', 'InstitutionsController')->only([
     'index', 'show'
 ]);
